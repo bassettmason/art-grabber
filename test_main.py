bafba192b88c1ce22e33b5d5fe70d9c3c2194613
Mason Bassett <Bassettmason@gmail.com>
2023-08-23 00:25:35 +0000

    no test but dummy

--- test_main.py
+++ test_main.py
@@ -1,6 +1,9 @@
-# import os
-# import pytest
+import os
+import pytest
 # from functions_framework import create_app
+
+def test_dummy():
+    assert 1 == 1, "Dummy test failed"
 
 # # Mocking the SecretManagerServiceClient
 # class MockSecretManagerServiceClient:
